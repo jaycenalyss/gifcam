bf061e17330385ba395b9a0f7553272ee5565b8d
jaycenalyss <steffantucker@gmail.com>
2017-05-10 22:24:19 -0500

    Flask again
    lerned2flask

--- webpages.py
+++ webpages.py
@@ -2,14 +2,13 @@ from flask import Flask
 import os
 app = Flask(__name__)
 
-gifdirectory = '/home/pi/gifcam/gifs/'
-
 @app.route('/')
 def index():
     images = ''
-    for filename in os.listdir(gifdirectory):
-        if filename.endswith(".gif"):
-            images = images + '<a href="' + gifdirectory +  filename + '"><img src="gifs/'+filename + '" height=200 width=200 /></a>'
+    for root, dirs, files in os.walk('.'):
+    	for filename in [os.path.join(root,name) for name in files]:
+        	if filename.endswith(".gif"):
+            	images = images + '<a href="' + gifdirectory +  filename + '"><img src="gifs/'+filename + '" height=200 width=200 /></a>'
     if images != '':
         return images
     return '<h1> Sorry, no gifs exist </h1>'
